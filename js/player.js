cea9a2569a155aaf461842e5d0ea8cd84872ae52
Dikshitha0812 <71216869+Dikshitha0812@users.noreply.github.com>
2020-10-31 17:39:20 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,35 @@
+class Player{
+    constructor (){
+     this.name =null;
+     this.index=null;
+     this.distance=0;
+     this.rank=null;
+     
+
+    }
+    getPlayerCount(){
+        var dbref=db.ref('playerCount');
+        dbref.on("value",(data)=>{
+            playerCount=data.val()
+        })
+    }
+  update(){
+      var playerindex="players/player"+this.index;
+      db.ref(playerindex).set({
+          name:this.name,
+          distance:this.distance
+
+      })
+  }
+  updateCount(Count){
+      db.ref('/').update({
+           playerCount:Count
+      })
+  }
+  static  getInfo(){
+   var dbref=db.ref('players');
+   dbref.on("value",(data)=>{
+ allPlayers=data.val();
+   })
+   }
+}
